0e42335daab6dba2b77e8cb2e13fdb4886f86a22
Souris.CLOUD <me@souris.cloud>
2021-02-27 00:53:20 +0100

    zkouska mikrofonu

---
+++ config/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+Config['TickTime'] = 1000
+Config['Hud'] = true
+Config['HideMinimap'] = true
+Config['Stress'] = true
+Config['PlayerID'] = true
+Config['Fuel'] = true
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+name 'SourisDevWorkbench'
+description 'Testing environment'
+ui_page 'html/ui.html'
+author 'Souris.CLOUD'
+
+files {
+  'html/ui.html'
+}
+
+shared_scripts {
+  'config/config.lua'
+}
